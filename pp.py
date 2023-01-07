8ff310528718e519d2911bff61ff6e8eadbf5c0d
Naman <114794070+Namanzz@users.noreply.github.com>
2023-01-07 21:16:11 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,15 +1,14 @@
-for _ in range(int(input())):
-    n = int(input())
-    if 360%n == 0:
-        a1 = 'y' 
-    else:
-        a1 = 'n' 
-    if n <=360:
-        a2 = 'y'
-    else:
-        a2 = 'n' 
-    if n <= 26:
-        a3 = 'y'
-    else:
-        a3 = 'n' 
-    print(a1,a2,a3)
+n = int(input()) #giving the integer input in which we want to divide the peices of cake!
+if 360%n == 0:
+    a1 = 'y' 
+else:
+    a1 = 'n' 
+if n <=360:
+    a2 = 'y'
+else:
+    a2 = 'n' 
+if n <= 26:
+    a3 = 'y'
+else:
+    a3 = 'n' 
+print(a1,a2,a3)
